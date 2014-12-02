07ab3c4940b00af4a26b52993db231ed7cff4727
Jeff Jin <jeff@jeffjin.com>
2014-12-02 00:51:55 -0500

    timer with dynamic nav button implemented

--- ImageShare/ImageSearchViewController.swift
+++ ImageShare/ImageSearchViewController.swift
@@ -1,46 +1,61 @@
-//
-//  ViewController.swift
-//  TipCalculator
-//
-//  Created by Main Account on 7/7/14.
-//  Copyright (c) 2014 Razeware LLC. All rights reserved.
-//
-
 import UIKit
 
 class ImageSearchViewController: UIViewController {
-  
-  @IBOutlet var keywordField : UITextField!
-@IBOutlet var scalePctSlider : UISlider!
-
-
+    
+    @IBOutlet var keywordField : UITextField!
+    @IBOutlet var scalePctSlider : UISlider!
+    @IBOutlet var resultImageView: UIImageView!
+    @IBOutlet var navigationBar: UINavigationBar!
+    
     let imgService:IImageService = ImageService(conf: "")
+    var isPlaying:Bool = false
+    var stopButton: UIBarButtonItem!
+    var playButton: UIBarButtonItem!
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  @IBAction func scalePercentageChanged(sender : AnyObject) {
-       println("Percentage slider changed \(scalePctSlider.value)")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Pause, target: self, action:Selector("showRandomImage"))
+        playButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Play, target: self, action:Selector("showRandomImage"))
+        self.navigationItem.rightBarButtonItem = playButton
+    }
     
-  }
-  @IBAction func viewTapped(sender : AnyObject) {
-    println("View tapped")
-    keywordField.resignFirstResponder()
-  }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func scalePercentageChanged(sender : AnyObject) {
+        println("Percentage slider changed \(scalePctSlider.value)")
+        
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        println("View tapped")
+        keywordField.resignFirstResponder()
+    }
+    
     @IBAction func searchClicked(sender: AnyObject) {
         println("Search clicked")
         var imgView = imgService.searchImageFromGoogle!(keywordField.text)
         resultImageView.image = imgView
     }
     
-    @IBOutlet var resultImageView: UIImageView!
+    func showRandomImage() {
+        var timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("updateImage"), userInfo: nil, repeats: true)
+        if(isPlaying){
+            self.navigationItem.rightBarButtonItem = stopButton
+        }
+        else{
+            self.navigationItem.rightBarButtonItem = playButton
+        }
+        isPlaying = !isPlaying
+        println("is playing ... \(isPlaying)")
+    }
+    
+    func updateImage(){
+        println("searching for random image...")
+        var imgView = imgService.searchImageFromGoogle!(keywordField.text)
+        resultImageView.image = imgView
+    }
 }
 
--- ImageShare/ImageService.swift
+++ ImageShare/ImageService.swift
@@ -21,18 +21,26 @@ class ImageService : IImageService{
         var imgs = Array<ImgResource>()
         
         imgs.append(ImgResource(title:"Image 1", url: "http://upload.wikimedia.org/wikipedia/commons/b/b2/SNSD_Cooky_Phone.jpg"))
-        imgs.append(ImgResource(title:"Image 2", url: "http://ecache.ilbe.com/files/attach/new/20130212/4255758/39883822/797961634/383ec523c127c9debd63d134c7ee54d8.jpeg"))
+        imgs.append(ImgResource(title:"Image 2", url: "http://bestkpopwallpaper.com/wp-content/uploads/2013/10/after-school-best-kpop-images.jpg"))
         imgs.append(ImgResource(title:"Image 3", url: "http://3.bp.blogspot.com/-sg4RjBMhm9w/T5ZiWyHEZHI/AAAAAAAAKSQ/kNLljtod3T8/s1600/Kpop+star+120422_07.jpg"))
         imgs.append(ImgResource(title:"Image 4", url: "http://www.mtviggy.com/wp-content/gallery/kpop-2000s-list/kpop_missacrop_0.jpg"))
         imgs.append(ImgResource(title:"Image 5", url: "http://images6.fanpop.com/image/photos/33500000/-K-pop-kpop-4ever-33571582-1680-1050.jpg"))
         imgs.append(ImgResource(title:"Image 6", url: "http://www.soompi.com/es/wp-content/blogs.dir/8/files/2012/12/SNSD.jpg"))
+        imgs.append(ImgResource(title:"Image 7", url: "http://www.beatnik.ca/seven/Girls_Generation_sailor_suits_colored.png"))
+        imgs.append(ImgResource(title:"Image 8", url: "http://images5.fanpop.com/image/photos/26100000/Secret-kpop-girl-power-26130791-640-441.jpg"))
+        imgs.append(ImgResource(title:"Image 9", url: "http://dliubb1xeg9ue.cloudfront.net/wp-content/uploads/2013/12/secretmadonna.jpg"))
         
         return imgs
     }
     
+    func randomNumber (lower: Int , upper: Int) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
+    
     func searchImageFromGoogle(keyword:String) -> UIImage{
-        var resource = ImgResource(title:"Image 1", url: "http://upload.wikimedia.org/wikipedia/commons/b/b2/SNSD_Cooky_Phone.jpg")
-        return resource.uiImage
+        var images = getImages()
+        var num = randomNumber(0, upper: 8)
+        return images[num].uiImage
     }
 
 
