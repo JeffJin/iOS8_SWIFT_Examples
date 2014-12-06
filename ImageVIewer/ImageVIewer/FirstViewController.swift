3dc3230ae0a8fee81388c6e84f52ad9af3ba0a84
Jeff Jin <jeff@jeffjin.com>
2014-12-06 02:36:28 -0500

    folder structure refactoring

---
+++ ImageVIewer/ImageVIewer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ImageVIewer
+//
+//  Created by Coho on 2014-12-06.
+//  Copyright (c) 2014 Coho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
