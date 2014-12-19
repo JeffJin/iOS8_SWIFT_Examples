8d6b78ab9741e1cb5999ddec4b5edaf2377f6a7e
Jeff Jin <jeff@jeffjin.com>
2014-12-19 00:36:15 -0500

    added image loading logic

--- ImageVIewer/ImageVIewer/SecondViewController.swift
+++ ImageVIewer/ImageVIewer/SecondViewController.swift
@@ -18,34 +18,34 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var sortByImageType: UIButton!
     
-    @IBOutlet var imageContainer: UIView!
+    var imageButtonList:[UIButton] = []
     
     var imageService:ImageService = ImageService(conf:"database connection")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // Do any additional setup after loading the view, typically from a nib.
+        keywordTxt.delegate = self
+        
         var placeHolderImg = imageService.getPlaceholderImage()
         
-        let width:CGFloat = 1000
-        let height:CGFloat = 768
+        let width:CGFloat = 768
+        let height:CGFloat = 568
         
-        var view:UIView = UIView(frame: CGRectMake(0, 0, width, height))
+        var view:UIView = UIView(frame: CGRectMake(0, 100, width, height))
         self.view.addSubview(view)
         
         for i in 0 ... 1{
             for j in 0 ... 2{
                 var button:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
                 button.frame = CGRectMake(0, 0, 155, 155)
-                button.center = CGPointMake(CGFloat(110 + 160 * i), CGFloat(190 + 160 * j))
+                button.center = CGPointMake(CGFloat(110 + 160 * i), CGFloat(90 + 160 * j))
                 button.setTitle("placeholder", forState: UIControlState.Normal)
                 button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
                 button.setBackgroundImage(placeHolderImg, forState: UIControlState.Normal)
                 button.addTarget(self, action: "addTargetImage:", forControlEvents: UIControlEvents.TouchUpInside)
                 button.tag = i
-                
+                imageButtonList.append(button)
                 view.addSubview(button)
             }
         }
@@ -64,11 +64,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         //        UIView.animateWithDuration(1, animations: { self.backgroundImageView.alpha = 0 }, completion:{_ in
         //            self.backgroundImageView.image = self.imageResources[button.tag].uiImage
         //            UIView.animateWithDuration(0.4, animations: { self.backgroundImageView.alpha = 1 })
-        //        })
-        
-        
-        
-        
+        //        })       
     }
     
     
@@ -77,9 +73,15 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    //response to return key press
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
-        
         keywordTxt.resignFirstResponder()
+        
+        var imageResults = imageService.getOnlineImages()
+        for (index, imgButton) in enumerate(imageButtonList){
+            imgButton.setBackgroundImage(imageResults[index].uiImage, forState: UIControlState.Normal)
+        }
+        
         return true
         
     }
